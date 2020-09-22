f178bb9a58b095dd5ce475f6abbe776f1c8c6684
RuslanKasymov <ruslan.kassymov97@mail.ru>
2020-09-23 00:38:23 +0600

    Setting routing

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function (Request $request) {
+    return response('', Response::HTTP_NO_CONTENT);
 });
